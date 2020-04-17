3cc5b6a4a49436abd768bc1714a486eaa35d9567
Kuljeet-123 <kuljeetsingh951@gmail.com>
2020-04-17 23:57:54 +0530

    Speech-to-Emotion

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const Sentiment = require('sentiment')
 
-const port = 3000
+const port = process.env.PORT || 3000
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
